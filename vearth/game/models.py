7bd3be9a85ca8f508ec9a6c9efc5292905abc25c
Nigel Gott <nigel.gott@gmail.com>
2016-07-17 14:58:59 +0100

    use a composite field for coords

--- vearth/game/models.py
+++ vearth/game/models.py
@@ -1,11 +1,18 @@
+from composite_field import CompositeField
 from django.db import models
+
+class CoordField(CompositeField):
+    x = models.FloatField()
+    y = models.FloatField()
+
+    def __str__(self):
+        return "Coord(x={},y={})".format(self.x, self.y)
 
 
 # Create your models here.
 class Human(models.Model):
-    x = models.IntegerField()
-    y = models.IntegerField()
+    coord = CoordField();
     name = models.CharField(max_length=50)
 
     def __str__(self):
-        return "Human(name={},x={},y={})".format(self.name, self.x, self.y)
+        return "Human(name={},coord={})".format(self.name, self.coord)
